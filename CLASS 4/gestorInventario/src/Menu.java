eeca0c0feee09ba2fa9de144734692eb0711be23
OscarPreciadoDev <oscarpreciadodev@gmail.com>
2026-07-02 11:51:07 -0500

    Inventario con soporte para varios productos

--- CLASS 4/gestorInventario/src/Menu.java
+++ CLASS 4/gestorInventario/src/Menu.java
@@ -7,9 +7,9 @@ public class Menu {
 
     public static void mostrar(){
         System.out.println();
-        System.out.println("||||| Gestor de inventario |||||");
+        System.out.println("***  GESTOR DE INVENTARIO ***");
         System.out.println("1. Registrar producto");
-        System.out.println("2. Ver producto");
+        System.out.println("2. Listar Inventario");
         System.out.println("3. Actualizar cantidad");
         System.out.println("4. Ver valor total");
         System.out.println("5. Salir");
--- CLASS 4/src/App.java
+++
@@ -1,3 +0,0 @@
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
